55a76c0222f7618b748ab38e9d56d1949fbeff92
janinestang <helenejs@stud.ntnu.no>
2018-08-13 16:54:44 +0200

    testing error detection / debugging

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person.js';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary.js';
 
 class App extends Component {
   state = {
@@ -60,12 +61,14 @@ class App extends Component {
           <div> 
             {this.state.persons.map((person,index) => {
               // JSX objects
-              return <Person 
-              click ={() => this.deletePersonHandler(index)}
-                name={person.name} 
-                age={person.age} 
-                key={person.id} 
-                changed={(event) => this.nameChangedHandler(event, person.id)}/>
+              return <ErrorBoundary key={person.id}> 
+                <Person 
+                    click ={() => this.deletePersonHandler(index)}
+                    name={person.name} 
+                    age={person.age} 
+                    key={person.id} 
+                    changed={(event) => this.nameChangedHandler(event, person.id)}/>
+                </ErrorBoundary>
             })}
           </div>
         );
